6f08387f62fe6e9789198b53cea3e257edda1e2d
cubuzoa <huseyinbabal88@gmail.com>
2013-12-20 08:42:33 +0200

    Base package renamed

---
+++ src/main/java/com/cubuzoa/springelasticsearchadmin/IndicesController.java
@@ -0,0 +1,15 @@
+package com.cubuzoa.springelasticsearchadmin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndicesController {
+	@RequestMapping(method = RequestMethod.GET, value = {"/indices"})
+	public String indices(ModelMap model) {
+		model.addAttribute("message", "Hello world!");
+		return "indices/layout";
+	}
+}
